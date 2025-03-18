29353331070dc83124eed45cf3e7029b703898e2
ypc2002 <2710782659@qq.com>
2025-03-18 20:03:43 +0800

    stark

---
+++ src/main/java/starkTest/tt.scala
@@ -0,0 +1,27 @@
+package starkTest
+import dbis.stark._
+import org.apache.spark.SparkContext
+import org.apache.spark.SpatialRDD._
+import org.apache.spark.api.java.JavaSparkContext
+//import org.apache.spark.api.java.JavaSparkContext
+
+
+object  tt {
+
+  import dbis.stark._
+  import org.apache.spark.SparkContext
+  import org.apache.spark.SpatialRDD._
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+    val sc =new SparkContext("local[*]", "test")
+    val countries = sc.textFile("D:\\whu\\baselines4spatialJoin-master\\data\\GridMesaData\\parks_id_100k.csv") // assume Schema ID;Name;WKT String
+      .map(line => line.split("\t"))
+      .map(arr => (STObject(arr(1)), arr(0).toInt )) // ( STObject, (Int, String) )
+
+    // find all geometries that contain the given point
+    val filtered = countries.contains(STObject("POINT( 8.474516 53.20708 )"))
+    println(filtered.count())
+  }
+
+}
